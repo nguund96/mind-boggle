e2fd9c9ac8b9abbc18175a62d53177e896933fa9
nguund96 <nguund.ute@gmail.com>
2019-06-15 14:14:27 +0700

    mind-boggle

---
+++ MindBoggle/MindBoggle/Form1.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace MindBoggle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_05 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_10 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_15 = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMI_Info = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.TSSL_Time = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tm_Time = new System.Windows.Forms.Timer(this.components);
+            this.pnl_Control = new System.Windows.Forms.Panel();
+            this.TSMI_Data = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(520, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.playToolStripMenuItem,
+            this.TSMI_Data,
+            this.TSMI_Exit});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // playToolStripMenuItem
+            // 
+            this.playToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMI_05,
+            this.TSMI_10,
+            this.TSMI_15});
+            this.playToolStripMenuItem.Name = "playToolStripMenuItem";
+            this.playToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.playToolStripMenuItem.Text = "Play";
+            // 
+            // TSMI_05
+            // 
+            this.TSMI_05.Name = "TSMI_05";
+            this.TSMI_05.Size = new System.Drawing.Size(109, 22);
+            this.TSMI_05.Text = "5 x 5";
+            this.TSMI_05.Click += new System.EventHandler(this.TSMI_05_Click);
+            // 
+            // TSMI_10
+            // 
+            this.TSMI_10.Name = "TSMI_10";
+            this.TSMI_10.Size = new System.Drawing.Size(109, 22);
+            this.TSMI_10.Text = "10 x 10";
+            this.TSMI_10.Click += new System.EventHandler(this.TSMI_05_Click);
+            // 
+            // TSMI_15
+            // 
+            this.TSMI_15.Name = "TSMI_15";
+            this.TSMI_15.Size = new System.Drawing.Size(109, 22);
+            this.TSMI_15.Text = "15 x 15";
+            this.TSMI_15.Click += new System.EventHandler(this.TSMI_05_Click);
+            // 
+            // TSMI_Exit
+            // 
+            this.TSMI_Exit.Name = "TSMI_Exit";
+            this.TSMI_Exit.Size = new System.Drawing.Size(152, 22);
+            this.TSMI_Exit.Text = "Exit";
+            this.TSMI_Exit.Click += new System.EventHandler(this.TSMI_Exit_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSMI_Info});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // TSMI_Info
+            // 
+            this.TSMI_Info.Name = "TSMI_Info";
+            this.TSMI_Info.Size = new System.Drawing.Size(152, 22);
+            this.TSMI_Info.Text = "Info";
+            this.TSMI_Info.Click += new System.EventHandler(this.TSMI_Info_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TSSL_Time});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 544);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(520, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // TSSL_Time
+            // 
+            this.TSSL_Time.Name = "TSSL_Time";
+            this.TSSL_Time.Size = new System.Drawing.Size(82, 17);
+            this.TSSL_Time.Text = "Time: 00:00:00";
+            // 
+            // tm_Time
+            // 
+            this.tm_Time.Interval = 1000;
+            this.tm_Time.Tick += new System.EventHandler(this.tm_Time_Tick);
+            // 
+            // pnl_Control
+            // 
+            this.pnl_Control.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnl_Control.Location = new System.Drawing.Point(0, 24);
+            this.pnl_Control.Name = "pnl_Control";
+            this.pnl_Control.Size = new System.Drawing.Size(520, 520);
+            this.pnl_Control.TabIndex = 2;
+            // 
+            // TSMI_Data
+            // 
+            this.TSMI_Data.Name = "TSMI_Data";
+            this.TSMI_Data.Size = new System.Drawing.Size(152, 22);
+            this.TSMI_Data.Text = "Data";
+            this.TSMI_Data.Click += new System.EventHandler(this.TSMI_Data_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(520, 566);
+            this.Controls.Add(this.pnl_Control);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mind Boggle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_05;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_10;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_15;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_Exit;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_Info;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel TSSL_Time;
+        private System.Windows.Forms.Timer tm_Time;
+        private System.Windows.Forms.Panel pnl_Control;
+        private System.Windows.Forms.ToolStripMenuItem TSMI_Data;
+
+
+
+    }
+}
+
---
+++ MindBoggle/MindBoggle/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MindBoggle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int ss = 0, mm = 0, hh = 0, _Size = 0;
+        Button[,] Buttons;
+        TimeSpan[] ts = new TimeSpan[3];
+
+        private void tm_Time_Tick(object sender, EventArgs e)
+        {
+            ss++;
+            if (ss == 60)
+            {
+                ss = 0; mm++;
+            }
+            if (mm == 60)
+            {
+                mm = 0; hh++;
+            }
+            string str = (hh > 9 ? hh.ToString() : ("0" + hh.ToString())) + ":" + (mm > 9 ? mm.ToString() : ("0" + mm.ToString())) + ":" + (ss > 9 ? ss.ToString() : ("0" + ss.ToString()));
+            TSSL_Time.Text = "Time: " + str;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                FileStream fs = new FileStream("Data.txt", FileMode.Open, FileAccess.Read, FileShare.None);
+                StreamReader sr = new StreamReader(fs);
+                for (int i = 0; i < 3; i++)
+                {
+                    string str = sr.ReadLine();
+                    string[] mang = str.Split(':');
+                    ts[i] = new TimeSpan(Convert.ToInt32(mang[0]), Convert.ToInt32(mang[1]), Convert.ToInt32(mang[2]));
+                }
+                sr.Close();
+                fs.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Không thể load dữ liệu!", "Thông báo");
+            }
+        }
+        public void CreateTable()
+        {
+            ss = mm = hh = 0;
+            TSSL_Time.Text = "Time: 00:00:00";
+            pnl_Control.Controls.Clear();
+            int x = 0, y = 0, dem = 1;
+            int Width_Height_Button = pnl_Control.Width / _Size;
+            Buttons = new Button[_Size, _Size];
+            for (int i = 0; i < _Size; i++)
+            {
+                for (int j = 0; j < _Size; j++)
+                {
+                    Buttons[i, j] = new Button();
+                    Buttons[i, j].Click += new System.EventHandler(this.Button_Click);
+                    Buttons[i, j].Width = Buttons[i, j].Height = Width_Height_Button;
+                    Buttons[i, j].BackColor = Color.SkyBlue;
+                    Buttons[i, j].Text = dem.ToString();
+                    Buttons[i, j].Name = dem.ToString();
+                    dem++;
+                }
+            }
+            for (int i = 0; i < _Size; i++)
+            {
+                x = 0;
+                y = i * Width_Height_Button;
+                for (int j = 0; j < _Size; j++)
+                {
+                    x = j * Width_Height_Button;
+                    Point p = new Point(x, y);
+                    Buttons[i, j].Location = p;
+                    pnl_Control.Controls.Add(Buttons[i, j]);
+                }
+            }
+            tm_Time.Start(); //Bắt đầu chơi
+        }
+        public void Button_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            Button Top = GetButton_Top(b), Bot = GetButton_Bot(b), Left = GetButton_Left(b), Right = GetButton_Right(b);
+            if (Top != null && Bot != null && Left != null && Right != null)
+            {
+                if (b.BackColor == Color.Red)
+                {
+                    if (Top.BackColor == Color.Red && Bot.BackColor == Color.Red && Left.BackColor == Color.Red && Right.BackColor == Color.Red)
+                    {
+                        Top.BackColor = Color.SkyBlue;
+                        Bot.BackColor = Color.SkyBlue;
+                        Left.BackColor = Color.SkyBlue;
+                        Right.BackColor = Color.SkyBlue;
+                        b.BackColor = Color.SkyBlue;
+                    }
+                }
+                else
+                {
+                    if (Top.BackColor == Color.SkyBlue && Bot.BackColor == Color.SkyBlue && Left.BackColor == Color.SkyBlue && Right.BackColor == Color.SkyBlue)
+                    {
+                        Top.BackColor = Color.Red;
+                        Bot.BackColor = Color.Red;
+                        Left.BackColor = Color.Red;
+                        Right.BackColor = Color.Red;
+                        b.BackColor = Color.Red;
+                    }
+                }
+            }
+            if(Win())
+            {
+                SaveData();
+                pnl_Control.Enabled = false;
+                tm_Time.Stop();
+                MessageBox.Show("You Winner!");
+                Form1_Load(this, new EventArgs());
+            }
+        }
+        public bool Win()
+        {
+            for (int i = 0; i < _Size; i++)
+            {
+                for (int j = 0; j < _Size; j++)
+                {
+                    if (Buttons[i, j].BackColor == Color.SkyBlue)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        public void SaveData()
+        {
+            FileStream fs = new FileStream("Data.txt", FileMode.Create, FileAccess.Write, FileShare.None);
+            StreamWriter sw = new StreamWriter(fs);
+
+            TimeSpan t = new TimeSpan(hh, mm, ss);
+            if(_Size == 5)
+            {
+                if(ts[0] < t)
+                {
+                    ts[0] = t;
+                }
+            }
+            else if(_Size == 10)
+            {
+                if (ts[1] < t)
+                {
+                    ts[1] = t;
+                }
+            }
+            else if(_Size == 15)
+            {
+                if (ts[2] < t)
+                {
+                    ts[2] = t;
+                }
+            }
+
+            sw.WriteLine(ts[0].ToString());
+            sw.WriteLine(ts[1].ToString());
+            sw.WriteLine(ts[2].ToString());
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+        }
+        public Button GetButton_Left(Button b)
+        {
+            int STT = Convert.ToInt32(b.Text);
+            int STT_Left = 0;
+            if (STT % _Size == 1) //Button b ở lề bên trái
+            {
+                STT_Left = STT + _Size - 1;
+            }
+            else
+            {
+                STT_Left = STT - 1;
+            }
+            foreach (Button bt in Buttons)
+            {
+                if (Convert.ToInt32(bt.Text) == STT_Left)
+                {
+                    return bt;
+                }
+            }
+            return null;
+        }
+        public Button GetButton_Right(Button b)
+        {
+            int STT = Convert.ToInt32(b.Text);
+            int STT_Right = 0;
+            if (STT % _Size == 0) //Button b ở lề bên phải
+            {
+                STT_Right = STT - _Size + 1;
+            }
+            else
+            {
+                STT_Right = STT + 1;
+            }
+            foreach (Button bt in Buttons)
+            {
+                if (Convert.ToInt32(bt.Text) == STT_Right)
+                {
+                    return bt;
+                }
+            }
+            return null;
+        }
+        public Button GetButton_Top(Button b)
+        {
+            int STT = Convert.ToInt32(b.Text);
+            int STT_Top = 0;
+            if ((STT - _Size) <= 0) //Button b ở lề bên trên
+            {
+                STT_Top = STT + _Size * (_Size - 1);
+            }
+            else
+            {
+                STT_Top = STT - _Size;
+            }
+            foreach (Button bt in Buttons)
+            {
+                if (Convert.ToInt32(bt.Text) == STT_Top)
+                {
+                    return bt;
+                }
+            }
+            return null;
+        }
+        public Button GetButton_Bot(Button b)
+        {
+            int STT = Convert.ToInt32(b.Text);
+            int STT_Bot = 0;
+            if ((STT + _Size) > _Size * _Size) //Button b ở lề bên dưới
+            {
+                STT_Bot = STT - _Size * (_Size - 1);
+            }
+            else
+            {
+                STT_Bot = STT + _Size;
+            }
+            foreach (Button bt in Buttons)
+            {
+                if (Convert.ToInt32(bt.Text) == STT_Bot)
+                {
+                    return bt;
+                }
+            }
+            return null;
+        }
+        private void TSMI_05_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem item = (ToolStripMenuItem)sender;
+            _Size = Convert.ToInt32(item.Name.Substring(5));
+            CreateTable();
+            pnl_Control.Enabled = true;
+        }
+
+        private void TSMI_Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void TSMI_Info_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mind Boggle\nNguyễn Đức Ngưu, ĐH SPKT TPHCM\n1.0.0", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void TSMI_Data_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("5x5:     " + ts[0].ToString() + "\n10x10: " + ts[1].ToString() + "\n15x15: " + ts[2].ToString());
+        }
+
+    }
+}
